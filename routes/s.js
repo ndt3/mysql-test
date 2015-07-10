d56bbd1597cab13626c97f7fc9677fc32d52e72d
Sanghwa Jung <jjhangu@naver.com>
2015-07-10 09:17:27 +0900

    test

--- routes/s.js
+++ routes/s.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET s */
+router.get('/insert/user', function(req, res, next) {
+  res.send('this first time to check url pattern');
+});
+
 module.exports = router;
