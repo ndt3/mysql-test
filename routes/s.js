7a50acc44694ad2e7b5892fd743b44da6cb91664
Sanghwa Jung <jjhangu@naver.com>
2015-08-07 09:57:21 +0900

    mysql modules added

--- routes/s.js
+++ routes/s.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,6 +12,136 @@ router.get('/', function(req, res, next) {
 /* GET s */
 router.get('/insert/user', function(req, res, next) {
   res.send('this first time to check url pattern');
+});
+
+
+router.get('/transactionTest/rollback', function(req, res, next) {
+  var connection = mysql.createConnection(
+      {
+        host     : 'localhost',
+        user     : 'node',
+        password : 'barusoft11',
+        database : 'node'
+      }
+  );
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+  connection.beginTransaction(function(err) {
+    if (err) { throw err; }
+    connection.query('INSERT INTO tb_board SET content=?', 'content ddd', function(err, result) {
+      console.log('result ' + result);
+
+      if (err) {
+        return connection.rollback(function() {
+          connection.end(function(err) {
+            // The connection is terminated now
+          });
+          throw err;
+        });
+      }
+
+      var log = 'Post ' + result.insertId + ' added';
+
+      connection.query('INSERT INTO log SET data=?', log, function(err, result) {
+        if (err) {
+          return connection.rollback(function() {
+            connection.end(function(err) {
+              // The connection is terminated now
+            });
+            throw err;
+          });
+        }
+        connection.commit(function(err) {
+          if (err) {
+            return connection.rollback(function() {
+              connection.end(function(err) {
+                // The connection is terminated now
+              });
+              throw err;
+            });
+          }else{
+            connection.end(function(err) {
+              // The connection is terminated now
+            });
+            console.log('success!');
+          }
+        });
+      });
+    });
+  });
+  res.send('transaction finished');
+});
+
+router.get('/transactionTest/commit', function(req, res, next) {
+
+  var connection = mysql.createConnection(
+      {
+        host     : 'localhost',
+        user     : 'node',
+        password : 'barusoft11',
+        database : 'node'
+      }
+  );
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+  });
+  connection.beginTransaction(function(err) {
+    if (err) { throw err; }
+    connection.query('INSERT INTO tb_board SET content=?', 'content ddd', function(err, result) {
+      if (err) {
+        return connection.rollback(function() {
+          connection.end(function(err) {
+            // The connection is terminated now
+          });
+          throw err;
+        });
+      }
+
+      console.log(result);
+
+      var log = 'Post ' + result.insertId + ' added';
+
+      var qu = 'INSERT INTO tb_board_reply (BOARD_SEQ, CONTENT)   VALUES (?, ?)';
+
+      connection.query(qu, [result.insertId, 'DDDED'], function(err, result) {
+        if (err) {
+          return connection.rollback(function() {
+            connection.end(function(err) {
+              // The connection is terminated now
+            });
+            throw err;
+          });
+        }
+        connection.commit(function(err) {
+          if (err) {
+            return connection.rollback(function() {
+              connection.end(function(err) {
+                // The connection is terminated now
+              });
+              throw err;
+            });
+          }else{
+            connection.end(function(err) {
+              // The connection is terminated now
+            });
+            console.log('success!');
+          }
+        });
+      });
+    });
+  });
+  res.send('transaction finished');
 });
 
 module.exports = router;
